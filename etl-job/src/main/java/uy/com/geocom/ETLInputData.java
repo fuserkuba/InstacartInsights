15bacaa1b3f31211309002175d02102fd44b7532
Damián Pérez Alfonso <atenxion@gmail.com>
2018-06-04 13:19:50 -0400

    Add etl-job and Spark dependency

---
+++ etl-job/src/main/java/uy/com/geocom/ETLInputData.java
@@ -0,0 +1,22 @@
+package uy.com.geocom;
+/**
+ * Hello world!
+ *
+ */
+public class ETLInputData
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+    private static void etl_instacart_to_geo_insight(String dataPath){
+        /*Dataset<Row> baskets = readDataSet(dataPath.concat(ORDERS_PRODUCTS_TRAIN)).union(dsOrdersProductsTrain).withColumnRenamed("order_id", "order");
+        baskets=baskets.withColumnRenamed("basketId","id");
+
+
+        Dataset<Row> products=dsProducts;*/
+
+    }
+}
